91f25ae5ece75ff3a56b791770f616798177f169
Luke C. Orth <luke.c.orth.civ@us.navy.mil>
2022-08-02 08:32:18 -0500

    Fixed some broken things with my neovim LSP configs

--- nvim/.config/nvim/after/plugin/lsp/init.lua
+++
@@ -1,2 +0,0 @@
-require('handlers').setup()
-require('lsp')
--- nvim/.config/nvim/lua/lukeo/packer.lua
+++ nvim/.config/nvim/lua/lukeo/packer.lua
@@ -1,10 +1,28 @@
 return require('packer').startup(function(use)
+    -- You know what it is...
     use 'wbthomason/packer.nvim'
+
+    -- Color palette
     use { 'catppuccin/nvim', as = 'catppuccin' }
+
+    -- NVIM main menu dashboard
     use 'glepnir/dashboard-nvim'
+
+    -- Lots of icons
     use 'ryanoasis/vim-devicons'
+
+    -- Babysittin' that tree
     use 'nvim-treesitter/nvim-treesitter'
+
+    -- Language server protocol
     use 'neovim/nvim-lspconfig'
     use 'williamboman/mason.nvim'
     use 'williamboman/mason-lspconfig.nvim'
+
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-nvim-lua'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'saadparwaiz1/cmp_luasnip'
 end)
